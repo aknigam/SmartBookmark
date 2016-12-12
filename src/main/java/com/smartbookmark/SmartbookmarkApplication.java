6438cba0db900f25c72cdb51bcb683c429315c1a
NIGAM, ANAND <a.nigam@cvent.com>
2016-12-12 10:49:06 +0530

    Initialised the repository

---
+++ src/main/java/com/smartbookmark/SmartbookmarkApplication.java
@@ -0,0 +1,12 @@
+package com.smartbookmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbookmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbookmarkApplication.class, args);
+	}
+}
