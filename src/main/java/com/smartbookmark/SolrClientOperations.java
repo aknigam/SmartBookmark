66d27d14f8de56584418a5e096e3b2fe772ab308
NIGAM, ANAND <a.nigam@cvent.com>
2017-01-27 21:15:08 +0530

    Solr integration

--- src/main/java/com/smartbookmark/SolrClientOperations.java
+++ src/main/java/com/smartbookmark/SolrClientOperations.java
@@ -4,9 +4,20 @@ import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.request.AbstractUpdateRequest;
+import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.util.ContentStreamBase;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.handler.extraction.ExtractingParams;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by a.nigam on 27/01/17.
@@ -35,7 +46,34 @@ public class SolrClientOperations {
         String queryString =  "*:*";
 
         SolrDocumentList docs =  sco.query(queryString);
-        System.out.println(docs.size());
+        System.out.println("No of docs after 30\t"+docs.size());
+
+        sco.indexFromUrl();
+
+    }
+
+    private void indexFromUrl() throws IOException, SolrServerException {
+
+        ContentStreamUpdateRequest up = new ContentStreamUpdateRequest("/update/extract");
+        up.addContentStream(new ContentStreamBase.URLStream(new URL("http://stackoverflow.com/questions/9452701/ukkonens-suffix-tree-algorithm-in-plain-english/9513423#9513423")));
+        up.setParam("literal.id", "stackoverflow3");
+        up.setAction(AbstractUpdateRequest.ACTION.COMMIT, true, true);
+//        NamedList<Object> result = solr.request(up);
+
+        solr.deleteById("stackoverflow1");
+        solr.deleteById("stackoverflow2");
+        solr.deleteById("stackoverflow3");
+
+        solr.commit();
+
+        QueryResponse rsp = solr.query(new SolrQuery("content:*Basic principle*"));
+        System.out.println(" No. of docs\t "+ rsp.getResults().getNumFound() );
+
+        for (int i = 0; i < rsp.getResults().getNumFound(); i++) {
+            SolrDocument result = rsp.getResults().get(i);
+            System.out.println(result.getFieldValue("id"));
+        }
+
     }
 
 
--- src/main/resources/solr/README.txt
+++ src/main/resources/solr/README.txt
@@ -1,10 +1,13 @@
 
 
 #stop command
+
 bin/solr stop -all
 
 #help
+
 bin/solr -help
+
 #command help
 bin/solr <command> -help
 #e.g bin/solr start -help
@@ -12,6 +15,8 @@ bin/solr <command> -help
 #creating a new core.
 #choose a solo home directory . Keep an empty sold.xml file there
 #run the following command
+
 bin/solr start -s /Users/a.nigam/Documents/workspace/smartbookmark/src/main/resources/solr
+
 #now create the core using following command
 bin/solr create -c smart bookmark
